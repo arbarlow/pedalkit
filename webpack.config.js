10287c40818099c5610c7c4340fa147aa00e6d53
Alex Barlow <alexbarlowis@gmail.com>
2017-07-24 22:42:06 +0100

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require("path");
+module.exports = {
+  entry: {
+    app: ["./src/entry.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+    {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+    },
+    {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+        ]
+    }
+    ]
+  }
+};
